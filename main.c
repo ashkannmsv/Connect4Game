193295ab9c813e59c1441ce3c633d3b145f6475b
ashkannmsv <ashkanmoosavi174@gmail.com>
2026-06-28 20:31:28 +0330

    bug fixed , player handeled

--- main.c
+++ main.c
@@ -10,37 +10,65 @@ int main() {
 
     clear_board(row , col , board);
 
-    for (int i=0 ; i<col ; i++)
-        printf("%d " , i+1);
-    printf("\t columns num\n\n");
-    print_board(row , col , board);
-    printf("enter the number of column: ");
+    // player chooser
+    printf("which player's turn? (1 or 2): ");
+    int player;
+    scanf("%d" , &player);
 
     while (!is_board_full(row , col , board))
     {
+        for (int i=0 ; i<col ; i++)
+            printf("%d " , i+1);
+        printf("\t columns num\n\n");
+        print_board(row , col , board);
+
+        while (player != 1 && player != 2) {
+            printf("the number you entered is not valid. please enter 1 or 2 :\n");
+            scanf("%d" , &player);
+        }
+
+
         //input
         int entered_num;
         scanf("%d" , &entered_num);
         entered_num--;
-        if (entered_num < 0 || entered_num > col) {
+        if (entered_num < 0 || entered_num >= col) {
             printf("the number you entered is not valid. please enter a number between 1 to %d :\n" , col);
             continue;
         }
-        input(row , col , board , 1 , entered_num);
+        
+        if (board[0][entered_num] != 0) {
+            printf("the column you entered is full. please enter another column :\n");
+            continue;
+        }
+        input(row , col , board , player , entered_num);
 
         //calc
-        if (is_winner(row , col , board))    break;
+        if (is_winner(row , col , board)){
+            print_board(row , col , board);
+            printf("player %d is the winner!\n" , player);
+            break;
+        }
         
+        if(player == 1) 
+            player = 2;
+        else
+            player = 1;
+        
+
         //print
         for (int i=0 ; i<col ; i++)
             printf("%d " , i+1);
-        
+
         printf("\t columns num\n\n");
         print_board(row , col , board);
+
+        printf("\n\n\n");
     }
     
-
-    
+    if (is_board_full(row , col , board)) {
+        printf("the board is full. it's a draw!\n");
+    }
 
     return 0;
 }
